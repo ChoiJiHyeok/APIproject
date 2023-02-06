f377528a59e5ff35afc08e0d663583aeeba7190c
jungyeonwoo265 <oqpwe7@naver.com>
2023-02-06 15:33:45 +0900

    230206 API_test.py 신간 측정 추가

--- API_test.py
+++ API_test.py
@@ -1,9 +1,11 @@
 import requests
 import xmltodict as xmltodict
 import math
+import time
 
 content = []
 
+stat_time = time.time()
 key = 'cbbbb410eb3d4bfa88e79a9172862f'
 url = f'http://www.incheon.go.kr/dp/openapi/data?apicode=10&page=1&key={key}'
 data_total = int(xmltodict.parse(requests.get(url).content)['data']['totalCount'])
@@ -21,6 +23,9 @@ for page in range(1, total_page+1):
         data_month = i['histDate'][0]+i['histDate'][1]
         data_day=i['histDate'][2]+i['histDate'][3]
         date_summary = i['summary']
-        print(f'page: {data_page}쪽, 등록 number: {data_listnum}, '
+        print(f'등록 NO: {data_listnum}, '
               f'일자: {data_year}년 {data_month}월 {data_day}일, '
               f'내용: {date_summary}')
+end_time = time.time()
+
+print(end_time-stat_time)

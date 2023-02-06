34e41069a70b968b5d954daa4de685fd32c5d3bf
jungyeonwoo265 <oqpwe7@naver.com>
2023-02-06 15:14:05 +0900

    230206 API_test.py 추가

---
+++ API_test.py
@@ -0,0 +1,26 @@
+import requests
+import xmltodict as xmltodict
+import math
+
+content = []
+
+key = 'cbbbb410eb3d4bfa88e79a9172862f'
+url = f'http://www.incheon.go.kr/dp/openapi/data?apicode=10&page=1&key={key}'
+data_total = int(xmltodict.parse(requests.get(url).content)['data']['totalCount'])
+total_page = math.ceil(data_total/10)
+for page in range(1, total_page+1):
+    url = f'http://www.incheon.go.kr/dp/openapi/data?apicode=10&page={page}&key={key}'
+    content = requests.get(url).content
+    dict = xmltodict.parse(content)
+    data = dict['data']
+    data_page = data['page']
+    date_item = data['list']['item']
+    for i in date_item:
+        data_listnum = i['listNum']
+        data_year = i['histYear']
+        data_month = i['histDate'][0]+i['histDate'][1]
+        data_day=i['histDate'][2]+i['histDate'][3]
+        date_summary = i['summary']
+        print(f'page: {data_page}쪽, 등록 number: {data_listnum}, '
+              f'일자: {data_year}년 {data_month}월 {data_day}일, '
+              f'내용: {date_summary}')

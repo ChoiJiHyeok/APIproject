4da2333877aa7ad144805c22dfe2989a9da510d2
jungyeonwoo265 <oqpwe7@naver.com>
2023-02-07 20:18:39 +0900

    230207 가입, 로그인 기능

--- student.py
+++ student.py
@@ -5,10 +5,29 @@ from PyQt5 import uic
 import socket
 import threading
 from datetime import datetime
+import requests
+import xmltodict as xmltodict
+import math
+import time
 
 form_class = uic.loadUiType("main.ui")[0]
 svrip = 'localhost'
 port = 9000
+
+db_host = '10.10.21.105'
+db_port = 3306
+db_user = 'network'
+db_pw = 'aaaa'
+db = 'api'
+
+
+def db_execute(sql):
+    conn = p.connect(host=db_host, port=db_port, user=db_user, password=db_pw, db=db, charset='utf8')
+    c = conn.cursor()
+    c.execute(sql)
+    conn.commit()
+    conn.close()
+    return c.fetchall()
 
 
 class WindowClass(QMainWindow, form_class):
@@ -16,6 +35,8 @@ class WindowClass(QMainWindow, form_class):
         super().__init__()
         self.setupUi(self)
         self.stackedWidget.setCurrentIndex(0)
+
+        self.read_api()
 
         self.hbt_login.clicked.connect(self.login)
 
@@ -25,6 +46,32 @@ class WindowClass(QMainWindow, form_class):
         print('연결된 서버:' + svrip)
         th = threading.Thread(target=self.receive, args=(self.sock,))
         th.start()
+
+    # API 자료가 업데이트 돼면 DB자료 변경
+    def read_api(self):
+        key = 'cbbbb410eb3d4bfa88e79a9172862f'
+        url = f'http://www.incheon.go.kr/dp/openapi/data?apicode=10&page=1&key={key}'
+        data_total = int(xmltodict.parse(requests.get(url).content)['data']['totalCount'])
+        total_page = math.ceil(data_total / 10)
+        sql = f'select count(*) from learning_data;'
+        api = db_execute(sql)[0][0]
+        if api != data_total:
+            sql = 'delete from learning_data;'
+            db_execute(sql)
+            for page in range(1, total_page + 1):
+                url = f'http://www.incheon.go.kr/dp/openapi/data?apicode=10&page={page}&key={key}'
+                content = requests.get(url).content
+                dict = xmltodict.parse(content)
+                data = dict['data']
+                date_item = data['list']['item']
+                for i in date_item:
+                    data_listnum = i['listNum']
+                    data_year = i['histYear']
+                    data_month = i['histDate'][0] + i['histDate'][1]
+                    data_day = i['histDate'][2] + i['histDate'][3]
+                    date_summary = i['summary']
+                    spl = f'insert into learning_data values ({data_listnum},"{data_year}년 {data_month}월 {data_day}일","{date_summary}")'
+                    db_execute(spl)
 
     def receive(self, c):
         while True:
--- teacher.py
+++ teacher.py
@@ -1,10 +1,12 @@
-import pymysql as p
+import json
 import sys
+from tkinter import messagebox, Tk
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import socket
 import threading
 from datetime import datetime
+
 
 form_class = uic.loadUiType("main.ui")[0]
 svrip = 'localhost'
@@ -17,6 +19,7 @@ class WindowClass(QMainWindow, form_class):
         self.setupUi(self)
         self.stackedWidget.setCurrentIndex(0)
 
+        self.hbt_add.clicked.connect(self.signup)
         self.hbt_login.clicked.connect(self.login)
 
         # 서버 연결
@@ -28,26 +31,64 @@ class WindowClass(QMainWindow, form_class):
 
     def receive(self, c):
         while True:
-            rmsg = c.recv(1024).decode()
+            rmsg = json.loads(c.recv(1024).decode())
             if rmsg:
                 self.p_msg(c, '받은 메시지:', rmsg)
-                self.reaction(c, rmsg)
+                self.reaction(c, rmsg[0], rmsg[1])
 
 ###########################################################################
 
     def login(self):
-        self.sock.sendall('login'.encode())
+        code = self.hle_code.text()
+        name = self.hle_name.text()
+        if code and name:
+            self.send_msg('login', [code, '관리자', name])
+        else:
+            self.messagebox('로그인 실패')
+        self.hle_code.clear()
+        self.hle_name.clear()
+
+    def signup(self):
+        name = self.hle_add_name.text()
+        admin = self.hrb_admin.isChecked()
+        user = self.hrb_user.isChecked()
+        if admin:
+            self.send_msg('signup', ['관리자', name])
+        elif user:
+            self.send_msg('signup', ['학생', name])
+        self.hle_add_name.clear()
 
 ###########################################################################
 
-    def reaction(self, c, msg):
-        pass
-
+    def reaction(self, c, head, msg):
+        print(head, msg)
+        if head == 'login':
+            if msg[0] == 'success':
+                self.stackedWidget.setCurrentIndex(2)
+                self.code = msg[1]
+                self.name = msg[2]
+                self.messagebox('로그인 성공')
+            else:
+                self.messagebox('로그인 실패')
+        elif head == 'signup':
+            if msg[0] == 'success':
+                code = msg[1]
+                self.messagebox(f'가입 성공, 발급 코드: {code} 입니다.')
+            else:
+                self.messagebox('가입 실패')
 
 ###########################################################################
 
-    def send_msg(self, head, *value):
-        self.sock.sendall(head.encode())
+    def messagebox(self, value):
+        tk_window = Tk()
+        tk_window.geometry("0x0+3000+6000")
+        messagebox.showinfo('안내창', f'{value}')
+        tk_window.destroy()
+
+    def send_msg(self, head, value):
+        msg = json.dumps([head, value])
+        self.sock.sendall(msg.encode())
+        self.p_msg(self.sock, '보낸 메시지:', msg)
 
     def p_msg(self, sock, head, *msg):
         if msg:

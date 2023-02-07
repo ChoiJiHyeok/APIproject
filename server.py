01b617e408554b70d5c55217d0fa5788903d20fe
jungyeonwoo265 <oqpwe7@naver.com>
2023-02-07 20:18:39 +0900

    230207 가입, 로그인 기능

--- server.py
+++ server.py
@@ -1,8 +1,25 @@
+import pymysql as p
 import socketserver
 from datetime import datetime
+import json
 
 server_ip = 'localhost'
 server_port = 9000
+
+db_host = '10.10.21.105'
+db_port = 3306
+db_user = 'network'
+db_pw = 'aaaa'
+db = 'api'
+
+
+def db_execute(sql):
+    conn = p.connect(host=db_host, port=db_port, user=db_user, password=db_pw, db=db, charset='utf8')
+    c = conn.cursor()
+    c.execute(sql)
+    conn.commit()
+    conn.close()
+    return c.fetchall()
 
 
 # 소켓 요청 처리
@@ -27,10 +44,10 @@ class MainServer:
     def receive(self, c):
         while True:
             try:
-                rmsg = c.recv(1024).decode()
+                rmsg = json.loads(c.recv(1024).decode())
                 if rmsg:
                     self.p_msg(c, '받은 메시지:', rmsg)
-                    self.reaction(c, rmsg)
+                    self.reaction(c, rmsg[0], rmsg[1])
             except ConnectionResetError:
                 self.p_msg(c, '연결 비정상 종료')
                 self.c_socks.remove(c)
@@ -41,21 +58,41 @@ class MainServer:
 
 ############################################################################
 
-    def reaction(self, c, msg):
-        pass
-
+    def reaction(self, c, head, msg):
+        print(head, msg)
+        if head == 'login':
+            sql = f"select * from login_data where member_num = '{msg[0]}' and authority = '{msg[1]}' and member_name='{msg[2]}';"
+            login = db_execute(sql)
+            if login:
+                self.send_msg(c, 'login', ['success', msg[0], msg[2]])
+            else:
+                self.send_msg(c, 'login', ['failure'])
+        elif head == 'signup':
+            if msg[0] == '관리자':
+                sql = "select count(*) from login_data where member_num like 'a%';"
+                num = int(db_execute(sql)[0][0])+1
+                sql = f"insert into login_data values('a{num}', '{msg[0]}', '{msg[1]}')"
+                db_execute(sql)
+                self.send_msg(c, 'signup', ['success', f'a{num}'])
+            else:
+                sql = "select count(*) from login_data where member_num like 's%';"
+                num = int(db_execute(sql)[0][0])+1
+                sql = f"insert into login_data values('a{num}', '{msg[0]}',' {msg[1]}')"
+                db_execute(sql)
+                self.send_msg(c, 'signup', ['success', f's{num}'])
 
 ############################################################################
+
+    def send_msg(self, c, head, value):
+        msg = json.dumps([head, value])
+        c.sendall(msg.encode())
+        self.p_msg(c, '보낸 메시지:', value)
 
     def p_msg(self, sock, head, *msg):
         if msg:
             print(f'{datetime.now()} / {sock.getpeername()} / {head} {msg}')
         else:
             print(f'{datetime.now()} / {sock.getpeername()} / {head}')
-
-    def send_msg(self, c, msg):
-        c.sendall(msg.encode())
-        self.p_msg(c, '보낸 메시지:', msg)
 
 
 if __name__ == '__main__':
